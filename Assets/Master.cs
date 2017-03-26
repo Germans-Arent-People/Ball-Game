2d63d196d5d0d01b3291640ac9a06ec447c2df91
Warren Wheeler <waw179@msstate.edu>
2017-03-26 16:28:57 -0500

    Initial Unity stuff

---
+++ Assets/Master.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Master : MonoBehaviour {
+
+	private bool chicken;
+	private Color dick;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+		
+}
